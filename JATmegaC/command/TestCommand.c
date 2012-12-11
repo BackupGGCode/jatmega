beddde04d47df212e03dddcab33bebe50f2449e3
radomir@mazon.pl <radomir@mazon.pl@46ccc553-50af-2b11-5b18-ff732a00c22a>
2012-12-11 12:30:08 +0000

    dodanie wiadomosci synchronicznych

--- JATmegaC/command/TestCommand.c
+++ JATmegaC/command/TestCommand.c
@@ -13,13 +13,23 @@
 extern Message_t requestBuffer[MESSAGE_BUFFER_SIZE];
 extern Message_t responseBuffer[MESSAGE_BUFFER_SIZE];
 
+/**
+ * Komenda testowa jest synchroniczna, oznacza to ze nadawca czeka na odpowiedz.
+ * Aby mozliwe bylo parowanie odpowiedzi z odpowiednim pytaniem w operandzie 0
+ * zawsze zapisany jest zpecjalny numer ramki, ktory trzeba zwrocic.
+ */
+
 void tc_apply(saf_Event event) {
 	if (event.code == COMMAND_TEST) {
-		responseBuffer[event.value].operand[0] = 0;
+		//zaznaczamy ze ta odpowiedz jest na konkretne pytanie
+		responseBuffer[event.value].operand[0] = requestBuffer[event.value].operand[0];
+
+		//logika:
+		responseBuffer[event.value].operand[1] = 0;
 		for (uint8_t i=0; i<requestBuffer[event.value].count; i++) {
-			responseBuffer[event.value].operand[0] += requestBuffer[event.value].operand[i];
+			responseBuffer[event.value].operand[1] += requestBuffer[event.value].operand[i];
 		}
-		responseBuffer[event.value].count = 1;
+		responseBuffer[event.value].count = 2;
 		saf_eventBusSend_(RESPONSE_TEST, event.value);
 	}
 }
