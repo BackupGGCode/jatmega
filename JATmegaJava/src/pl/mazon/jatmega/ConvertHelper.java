d9ccf8bab1475166205eb462a7541cacaafc56c1
radomir@mazon.pl <radomir@mazon.pl@46ccc553-50af-2b11-5b18-ff732a00c22a>
2012-12-11 12:34:21 +0000

    pierwsza wersja 2.0 , dziala juz TestCommand

---
+++ JATmegaJava/src/pl/mazon/jatmega/ConvertHelper.java
@@ -0,0 +1,25 @@
+package pl.mazon.jatmega;
+
+public class ConvertHelper {
+
+	public static String byteToHex(byte b) {
+		return String.format("%02X", b);
+	}
+	
+	public static String byteArrayToHex(byte[] byteArray) {
+		StringBuffer result = new StringBuffer();
+		for (byte b:byteArray) {
+		    result.append(String.format("%02X ", b));
+		}
+		return result.toString();
+	}
+
+	public static String ByteArrayToHex(Byte[] byteArray) {
+		StringBuffer result = new StringBuffer();
+		for (Byte b:byteArray) {
+		    result.append(String.format("%02X ", b));
+		}
+		return result.toString();
+	}
+
+}
