76385554b6987ddd3af6e08dadb879135db1c6b3
radomir@mazon.pl <radomir@mazon.pl@46ccc553-50af-2b11-5b18-ff732a00c22a>
2012-12-14 19:42:43 +0000

    2.0

--- JATmegaJava/src/pl/mazon/jatmega/core/command/TestCommand.java
+++ JATmegaJava/src/pl/mazon/jatmega/core/command/TestCommand.java
@@ -1,39 +1,53 @@
 package pl.mazon.jatmega.core.command;
 
-import pl.mazon.jatmega.core.model.IModel;
-import pl.mazon.jatmega.core.model.ByteModel;
+import pl.mazon.jatmega.core.model.MetaModel;
+
 
 /**
  * Test polega na przeliczeniu wartości
  * a i b. Wynik w odpowiedzi z atmega zawieta c=a+b
+ * lub a, b i c jesli wyslemy trzeci operand c=a+b+c
  * @author radomir.mazon
  */
 
-public abstract class TestCommand implements ICommand<ByteModel, ByteModel> {
+public abstract class TestCommand implements ICommand<MetaModel, MetaModel> {
 
-	private ByteModel request;
+	private MetaModel request;
 	
-	private ByteModel response;
+	private MetaModel response;
 	
-	public TestCommand(int a, int b) {
-		request = new ByteModel();
-		response = new ByteModel();
+	public TestCommand(byte a, byte b, byte c) {
+		request = new MetaModel();
+		response = new MetaModel();
+		request.add(a);
+		request.add(b);
+		request.add(c);
+	}
+	
+	public TestCommand(byte a, byte b) {
+		request = new MetaModel();
+		response = new MetaModel();
 		request.add(a);
 		request.add(b);
 	}
 	
 	@Override
-	public ByteModel getResponse() {
+	public MetaModel getResponse() {
 		return response;
 	}
 	
 	@Override
-	public ByteModel getRequest() {
+	public MetaModel getRequest() {
 		return request;
 	}
 
 	@Override
-	public char getTargetName() {
-		return 'X';
+	public byte getTargetName() {
+		return (byte)0x00;
+	}
+
+	@Override
+	public boolean isResponseMendatory() {
+		return true;
 	}
 }
